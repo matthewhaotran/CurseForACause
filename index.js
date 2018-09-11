0e508b12c6a501ce5563d4a688b1a0c6bb2ba32a
matthewhaotran@gmail.com <matthewhaotran@gmail.com>
2018-09-10 19:56:27 -0700

    added simple JS counter

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let x = 0.00;
+let balance = 0.00;
 
-$("h3").append("$" + x);
+$("h3").append("$" + balance.toFixed(2));
+
+$("#addToJar").click(function(){
+    balance += 0.25;
+    $("h3").text("$" + balance.toFixed(2));
+});
