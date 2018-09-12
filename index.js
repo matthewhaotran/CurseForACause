14cf77ce5c049c68b716243fda2710368c4def18
matthewhaotran <matthewhaotran@gmail.com>
2018-09-11 17:40:39 -0700

    connect to clearDB database

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 });
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-01.cleardb.net',
+  user     : 'ba2844b56811ac',
+  password : '0acae961',
+  database : 'heroku_f67894daeab864d'
+});
+
+connection.connect()
+
+connection.query('SELECT * FROM Balance;', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0]);
+})
+
+connection.end()
